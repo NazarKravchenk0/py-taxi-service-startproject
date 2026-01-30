0ddb16bc8005f3f183d632d32d213821a36298f1
nazarkravchenko <nazarnazarik5@gmail.com>
2026-01-30 13:24:02 +0200

    check

---
+++ taxi/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from taxi.models import Car, Driver, Manufacturer
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ("name", "country")
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    search_fields = ("model",)          # поиск по model
+    list_filter = ("manufacturer",)     # фильтр по manufacturer
+    list_display = ("model", "manufacturer")
+
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("license_number",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
